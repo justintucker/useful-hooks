328c6b149f9cae4ba6f45a31296cbd05a5202578
Justin Tucker <justin@certainstrings.com>
2019-06-16 15:28:17 -0600

    Add useEffectAfterMount hook

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Useful Hooks
+
 A collection of useful custom React hooks
 
-1. [useEffectAfterMount](#)
+1. [useEffectAfterMount](https://github.com/justintucker/useful-hooks/blob/master/hooks/useEffectAfterMount.js)
---
+++ hooks/useEffectAfterMount.js
@@ -0,0 +1,15 @@
+/**
+ * Replicate componentDidUpdate.
+ *
+ * @param {function} callback Function to run after update.
+ * @param {array} dependencies Dependencies used in the effect.
+ */
+const useEffectAfterMount = (callback, dependencies) => {
+  const isMounted = useRef(true);
+  useEffect(() => {
+    if (!isMounted.current) {
+      callback();
+    }
+    isMounted.current = false;
+  }, dependencies);
+};
